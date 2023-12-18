39da062502686ae3740a0a99b3bc0ceb5148976a
Gago Yerevan <karlenovich@yahoo.com>
2023-12-18 12:40:25 -0800

    change config

--- next.config.js
+++ next.config.js
@@ -2,17 +2,6 @@
 const ContentSecurityPolicy = require('./csp')
 const redirects = require('./redirects')
 
-module.exports = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Note: we provide webpack above so you should not `require` it
-    // Perform customizations to webpack config
-    config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
-
-    // Important: return the modified config
-    return config
-  },
-}
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
